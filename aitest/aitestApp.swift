9005056a8c2483f85ed6b2b142346773ba0deb54
dangered wolf <d@ngeredwolf.me>
2025-06-10 22:38:57 -0400

    Initial Commit

---
+++ aitest/aitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  aitestApp.swift
+//  aitest
+//
+//  Created by dangered wolf on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct aitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ aitestTests/aitestTests.swift
@@ -0,0 +1,17 @@
+//
+//  aitestTests.swift
+//  aitestTests
+//
+//  Created by dangered wolf on 6/10/25.
+//
+
+import Testing
+@testable import aitest
+
+struct aitestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
