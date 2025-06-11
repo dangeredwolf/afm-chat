f0bf96d7d0af4520736c1bda610cbd44cc603043
dangered wolf <d@ngeredwolf.me>
2025-06-11 01:14:52 -0400

    appclip target

---
+++ appclip/appclipApp.swift
@@ -0,0 +1,17 @@
+//
+//  appclipApp.swift
+//  appclip
+//
+//  Created by dangered wolf on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct appclipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
